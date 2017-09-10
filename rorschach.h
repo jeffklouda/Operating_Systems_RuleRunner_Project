c67e9f336d7d75f45c354b155547f5fd5b74bf2f
Matthew Anthony DAlonzo <mdalonz1@cushing63.helios.nd.edu>
2017-09-10 13:46:29 -0400

    slight alteration to rorschach.h

--- rorschach.h
+++ rorschach.h
@@ -1,9 +1,17 @@
 #ifndef RORSCHACH_H
 #define RORSCHACH_H
 
+#include <errno.h>
 #include <string>
 #include <sys/stat.h>
 #include <vector>
+#include <stdio.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
 
 using namespace std;
 
