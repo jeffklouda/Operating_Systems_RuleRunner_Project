dc0b6313ecd73648a8ac7ac922a7bbadb250d4de
Jeff Klouda <jklouda1@nd.edu>
2017-09-10 14:55:31 -0400

    Added regex functionality to check functions

--- check_functions.cpp
+++ check_functions.cpp
@@ -9,42 +9,63 @@
 #include <map>
 #include <vector>
 #include <string>
+#include <regex>
 
-void check_create(map<ino_t,file_scan> &prev_scan,
+using namespace std;
+
+void check_create(  map<ino_t,file_scan> &prev_scan,
                     map<ino_t,file_scan> &curr_scan,
                     vector<string> &rule) {
     
     for (auto it = curr_scan.begin(); it != curr_scan.end(); ++it) {
-        std::map<ino_t,file_scan>::iterator findIt;
+        map<ino_t,file_scan>::iterator findIt;
         findIt = prev_scan.find(it->first);
         if (findIt == prev_scan.end()) {
             // file created
+            string fileName = it->second.name;
+            regex regexName (rule[1]);
+            if (regex_match(fileName, regexName)) {
+                // run rule
+                run_commands(rule);
+            }
         }
     }   
 }
 
-void check_modify(map<ino_t,file_scan> &prev_scan,
+void check_modify(  map<ino_t,file_scan> &prev_scan,
                     map<ino_t,file_scan> &curr_scan,
                     vector<string> &rule) {
 
     for (auto it = curr_scan.begin(); it != curr_scan.end(); ++it) {
-        std::map<ino_t,file_scan>::iterator findIt;
+        map<ino_t,file_scan>::iterator findIt;
         findIt = prev_scan.find(it->first);
         if (findIt->second.lastMod != it->second.lastMod) {
             // file modified
+            string fileName = it->second.name;
+            regex regexName (rule[1]);
+            if (regex_match(fileName, regexName)) {
+                // run rule
+                run_commands(rule);
+            }
         }
     }
 }
 
-void check_delete(map<ino_t,file_scan> &prev_scan,
+void check_delete(  map<ino_t,file_scan> &prev_scan,
                     map<ino_t,file_scan> &curr_scan,
                     vector<string> &rule) {
 
     for (auto it = prev_scan.begin(); it != prev_scan.end(); ++it) {
-        std::map<ino_t,file_scan>::iterator findIt;
+        map<ino_t,file_scan>::iterator findIt;
         findIt = curr_scan.find(it->first);
         if (findIt == curr_scan.end()) {
             // file deleted
+            string fileName = it->second.name;
+            regex regexName (rule[1]);
+            if (regex_match(fileName, regexName)) {
+                // run rule
+                run_commands(rule);
+            }
         }
     }
 }
