b5bb4e53673d57939efa86a485f5cc8809fc3950
Jeff Klouda <jklouda1@nd.edu>
2017-09-10 14:20:22 -0400

    wrote check functions

--- check_functions.cpp
+++ check_functions.cpp
@@ -4,19 +4,47 @@
  * Authors: Matthew D'Alonzo and Jeff Klouda
  */
 
-#include "spidey.h"
+#include "rorschach.h"
+#include <sys/stat.h>
+#include <map>
+#include <vector>
+#include <string>
 
-void check_create(){
-
-
+void check_create(map<ino_t,file_scan> &prev_scan,
+                    map<ino_t,file_scan> &curr_scan,
+                    vector<string> &rule) {
+    
+    for (auto it = curr_scan.begin(); it != curr_scan.end(); ++it) {
+        std::map<ino_t,file_scan>::iterator findIt;
+        findIt = prev_scan.find(it->first);
+        if (findIt == prev_scan.end()) {
+            // file created
+        }
+    }   
 }
 
-void check_modify(){
+void check_modify(map<ino_t,file_scan> &prev_scan,
+                    map<ino_t,file_scan> &curr_scan,
+                    vector<string> &rule) {
 
-
+    for (auto it = curr_scan.begin(); it != curr_scan.end(); ++it) {
+        std::map<ino_t,file_scan>::iterator findIt;
+        findIt = prev_scan.find(it->first);
+        if (findIt->second.lastMod != it->second.lastMod) {
+            // file modified
+        }
+    }
 }
 
-void check_delete(){
+void check_delete(map<ino_t,file_scan> &prev_scan,
+                    map<ino_t,file_scan> &curr_scan,
+                    vector<string> &rule) {
 
-
+    for (auto it = prev_scan.begin(); it != prev_scan.end(); ++it) {
+        std::map<ino_t,file_scan>::iterator findIt;
+        findIt = curr_scan.find(it->first);
+        if (findIt == curr_scan.end()) {
+            // file deleted
+        }
+    }
 }
