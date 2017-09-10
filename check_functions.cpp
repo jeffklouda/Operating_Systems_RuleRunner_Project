4b96e50b7f844cfd6255ebd1a91c478758381181
Matthew Anthony DAlonzo <mdalonz1@cushing63.helios.nd.edu>
2017-09-10 13:34:33 -0400

    edited check_functions.cpp

---
+++ check_functions.cpp
@@ -0,0 +1,22 @@
+/* check_functions.cpp
+ * This contains the functions that check if a file has
+ * been CREATED, MODIFIED, or DELETED
+ * Authors: Matthew D'Alonzo and Jeff Klouda
+ */
+
+#include "spidey.h"
+
+void check_create(){
+
+
+}
+
+void check_modify(){
+
+
+}
+
+void check_delete(){
+
+
+}
